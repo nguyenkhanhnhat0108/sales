70b1f2db4d39724230b0eab8a38528fa7a92cec7
unknown <nhatnk01081993@gmail.com>
2020-02-03 21:07:57 +0700

    dd

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,30 +3,30 @@ import { connect } from "react-redux";
 import { logoutUser } from "../actions";
 
 class Home extends Component {
-    handleLogout = () => {
-        const { dispatch } = this.props;
-        dispatch(logoutUser());
-    };
-    render() {
-        const { isLoggingOut, logoutError, user } = this.props;
+  handleLogout = () => {
+    const { dispatch } = this.props;
+    dispatch(logoutUser());
+  };
+  render() {
+    const { isLoggingOut, logoutError, user } = this.props;
 
-        return (
-            <div>
-                <h1>This is your app's protected area.</h1>
-                <p>Any routes here will also be protected</p>
-                <p>{user.email}</p>
-                <button onClick={this.handleLogout}>Logout</button>
-                {isLoggingOut && <p>Logging Out....</p>}
-                {logoutError && <p>Error logging out</p>}
-            </div>
-        );
-    }
+    return (
+      <div>
+        <h1>This is your app's protected area.</h1>
+        <p>Any routes here will also be protected dddd</p>
+        <p>{user.email}</p>
+        <button onClick={this.handleLogout}>Logout</button>
+        {isLoggingOut && <p>Logging Out....</p>}
+        {logoutError && <p>Error logging out</p>}
+      </div>
+    );
+  }
 }
 function mapStateToProps(state) {
-    return {
-        isLoggingOut: state.auth.isLoggingOut,
-        logoutError: state.auth.logoutError,
-        user: state.auth.user
-    };
+  return {
+    isLoggingOut: state.auth.isLoggingOut,
+    logoutError: state.auth.logoutError,
+    user: state.auth.user
+  };
 }
 export default connect(mapStateToProps)(Home);
